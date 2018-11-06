41c5afb38f4efefa4e911b979ad2ef40d1204438
MIC <michaelvictorino@gmail.com>
2018-11-05 19:08:45 -0500

    Revert "Merge branch 'develop' into feature/Michael"
    This reverts commit 7ab5546dabeff843235acf425f9cee0d0755e5b4.

--- PH Engine/Engine/Shader.h
+++ PH Engine/Engine/Shader.h
@@ -1,94 +1,40 @@
 #pragma once
 
 #include <glad/glad.h>
-
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include "Math/math.h"
-//
+
 class Shader
 {
 public:
-	GLuint ID;
-	// constructor generates the shader on the fly
-	Shader() { }
-	// ------------------------------------------------------------------------
-	Shader(const char* vertexPath, const char* fragmentPath);
-	// activate the shader
-	// ------------------------------------------------------------------------
-	void use()
-	{
-		glUseProgram(ID);
-	}
-	//utility uniform functions
-	//------------------------------------------------------------------------
-	void setBool(const std::string &name, bool value) const
-	{
-		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-	}
-	// ------------------------------------------------------------------------
-	void setInt(const std::string &name, int value) const
-	{
-		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-	}
-	// ------------------------------------------------------------------------
-	void setFloat(const std::string &name, float value) const
-	{
-		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-	}
-	// ------------------------------------------------------------------------
-	void setVec2(const std::string &name, vec2 vec) const
-	{
-		glUniform2f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y);
-	}
-	// ------------------------------------------------------------------------
-	void setVec3(const std::string & name, vec3 vec) const
-	{
-		glUniform3f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y, vec.z);
-	}
-	void setVec3(const std::string &name, float x, float y, float z) const
-	{
-		glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
-	}
-	// ------------------------------------------------------------------------
-	void setVec4(const std::string & name, vec4 vec) const
-	{
-		glUniform4f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y, vec.z, vec.w);
-	}
-	void setVec3(const std::string &name, float x, float y, float z, float w) const
-	{
-		glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
-	}
-	void setMat4(const std::string & name, const mat4& mat) const
-	{
-		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat.m[0][0]);
-	}
+	Shader() {}
+	Shader(const char* vertPath, const char* fragPath);
+	~Shader() {}
+	void load(const char* vertPath, const char* fragPath);
+	void unload();
+	std::string readFile(const char* path);
+	void use();
+	void unuse();
+
+	int getAttribLocation(const std::string & location) const;
+	int getUniformLocation(const std::string & location) const;
+
+	void setAttribLocation(unsigned int index, const std::string &location);
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+	void setVec2(const std::string &name, vec2 vec) const;
+	void setVec3(const std::string & name, vec3 vec) const;
+	void setVec4(const std::string & name, vec4 vec) const;
+	void setMat4(const std::string & name, const mat4& mat) const;
+
 private:
-	// utility function for checking shader compilation/linking errors.
-	// ------------------------------------------------------------------------
-	void checkCompileErrors(unsigned int shader, std::string type)
-	{
-		int success;
-		char infoLog[1024];
-		if (type != "PROGRAM")
-		{
-			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-			if (!success)
-			{
-				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-			}
-		}
-		else
-		{
-			glGetProgramiv(shader, GL_LINK_STATUS, &success);
-			if (!success)
-			{
-				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-			}
-		}
-	}
+	GLuint ID;
+	GLuint vertex, fragment;
+	//std::string vertexCode;
+	//std::string fragmentCode;
+	void checkCompile(unsigned int shader, std::string type);
 };
