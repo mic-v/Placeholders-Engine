51c87ab3e43a6b1d11e82f18c10bf7c49c3e6f1b
Unknown <michaelvictorino@gmail.com>
2018-11-08 12:36:26 -0500

    3.21

---
+++ PH Engine/Engine/ResourceManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/*
+	ResourceManager class using singleton pattern
+	There is only one instance of this class and mostly all of its
+	functions are static
+
+*/
+
+class ResourceManager
+{
+public:
+
+
+
+private:
+
+	//
+	ResourceManager() {}
+}
